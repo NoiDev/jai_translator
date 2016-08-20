2bee854586fd93fa6ca3b0128cb069ceadd11512
Noah Mirovsky <Noi.Utility@gmail.com>
2016-08-20 12:54:43 -0500

    Generate output file.

--- jtrans.c
+++ jtrans.c
@@ -1,9 +1,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char *argv[]) {
-    char *file_name;
+    char *input_file_path;
     if (argc < 2) {
         printf("Must pass a file to translate.\n");
         return 1;
@@ -12,7 +13,25 @@ int main (int argc, char *argv[]) {
         printf("Please process one file at a time.\n");
         return 1;
     } else {
-        file_name = argv[1];
-        printf("File to process: %s\n", file_name);
+        input_file_path = argv[1];
+        printf("File to process: %s\n", input_file_path);
     }
+
+    int base_path_len = strchr(input_file_path, '.') - input_file_path + 1;
+
+    char *output_file_path = malloc(base_path_len + 4);
+    strncpy(output_file_path, input_file_path, base_path_len);
+    output_file_path[base_path_len] = 0;
+    strcat(output_file_path, "jai");
+
+    printf("Output file: %s\n", output_file_path);
+
+    FILE *output_file = NULL;
+    output_file = fopen(output_file_path, "w");
+    if (output_file) {
+        fprintf(output_file, "/* Output file generated for: %s", input_file_path);
+        fclose(output_file);
+    }
+
+    free(output_file_path);
 }
