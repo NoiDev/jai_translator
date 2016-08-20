8793c22a65721b31bb360893afb944ac66b40154
Noah Mirovsky <Noi.Utility@gmail.com>
2016-08-20 11:58:33 -0500

    Get input filename from command line.

--- jtrans.c
+++ jtrans.c
@@ -3,4 +3,16 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+    char *file_name;
+    if (argc < 2) {
+        printf("Must pass a file to translate.\n");
+        return 1;
+    } else if (argc > 2) {
+        printf("Only single file processing currently supported.\n");
+        printf("Please process one file at a time.\n");
+        return 1;
+    } else {
+        file_name = argv[1];
+        printf("File to process: %s\n", file_name);
+    }
 }
