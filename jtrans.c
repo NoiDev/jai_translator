c74cc3707567a2934021949536fea24d9c6aab7d
Noah Mirovsky <Noi.Utility@gmail.com>
2016-08-20 13:35:59 -0500

    Load input file to memory.

--- jtrans.c
+++ jtrans.c
@@ -17,6 +17,12 @@ int main (int argc, char *argv[]) {
         printf("File to process: %s\n", input_file_path);
     }
 
+    FILE *input_file = fopen(input_file_path, "r");
+    if (!input_file) {
+        printf("Unable to open input file for reading: %s", input_file_path);
+        exit(EXIT_FAILURE);
+    }
+
     int base_path_len = strchr(input_file_path, '.') - input_file_path + 1;
 
     char *output_file_path = malloc(base_path_len + 4);
@@ -34,8 +40,20 @@ int main (int argc, char *argv[]) {
     }
 
     fprintf(output_file, "/* Output file generated for: %s", input_file_path);
+
+    /* Load input file into memory. */
+    char *input_buffer;
+    fseek(input_file, 0L, SEEK_END);
+    int input_file_size = ftell(input_file);
+    fseek(input_file, 0L, SEEK_SET);
+    input_buffer = (char *)malloc(input_file_size + 1);
+    fread(input_buffer, input_file_size, 1, input_file);
+    input_buffer[input_file_size] = 0;
+    fclose(input_file);
+
     fclose(output_file);
 
+    free(input_buffer);
     free(output_file_path);
 
     exit(EXIT_SUCCESS);
