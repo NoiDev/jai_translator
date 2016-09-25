499032ad4272bc5ab9fa86e41d60dc7e8cacedde
Noah Mirovsky <Noi.Utility@gmail.com>
2016-09-25 14:30:30 -0500

    Save compiler output to log file.

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+
 rm -rf build
 mkdir build
-clang jtrans.c -o build/jtrans -O0 -g   ## Debug
-#clang jtrans.c -o build/jtrans -O2     ## Fast
+
+(
+
+    clang jtrans.c -o build/jtrans -O0 -g   2>&1    ## Debug
+    #clang jtrans.c -o build/jtrans -O2     2>&1    ## Fast
+
+) | tee "build/build.log";
